126489cf680ef28892f6d571dd5731602d2bce69
phú đặng <phubi268@gmail.com>
2023-11-06 16:04:14 +0700

    update api

--- src/service/axios/api.js
+++ src/service/axios/api.js
@@ -5,6 +5,13 @@ export const userAPI = {
   postLoginUser: async ({ taiKhoan, matKhau }) => {
     return https.post(`/api/QuanLyNguoiDung/DangNhap/`, { taiKhoan, matKhau });
   },
+  getTypeUser: async () => {
+    return https.get(`/api/QuanLyNguoiDung/LayDanhSachLoaiNguoiDung`);
+  },
+
+  postNewUser: async (user) => {
+    return https.post(`/api/QuanLyNguoiDung/ThemNguoiDung`, user);
+  },
 };
 
 export let adminService = {
@@ -25,5 +32,19 @@ export let adminService = {
   updateUser: (payload) => {
     console.log(payload);
     return https.post(`/api/QuanLyNguoiDung/CapNhatThongTinNguoiDung`, payload);
+  },
+};
+export const bannerAPI = {
+  getBanners: async () => {
+    return https.get(`/api/QuanLyPhim/LayDanhSachBanner`);
+  },
+};
+
+export const moviesAPI = {
+  // https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhimPhanTrang?maNhom=GP09&soTrang=1&soPhanTuTrenTrang=10
+  getMovies: async (soTrang = 1, soPhanTuTrenTrang = 10) => {
+    return https.get(
+      `/api/QuanLyPhim/LayDanhSachPhimPhanTrang?maNhom=GP09&soTrang=1&soPhanTuTrenTrang=10`
+    );
   },
 };
